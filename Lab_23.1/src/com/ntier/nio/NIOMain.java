44a6e360669acec4aa6a6c4b1c1b0d566f74c8ac
dalee2001 <63241267+dalee2001@users.noreply.github.com>
2020-04-06 11:14:55 -0700

    Initial commit for Lab 23.1

---
+++ Lab_23.1/src/com/ntier/nio/NIOMain.java
@@ -0,0 +1,21 @@
+package com.ntier.nio;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NIOMain {
+	public static void main(String[] args) {
+		Path pth = Paths.get("C:/Users/Dalee/eclipse-workspace\\Lab_23.1");
+		try {
+			DirectoryStream<Path> ds = Files.newDirectoryStream(pth, "a*");
+			for (Path currentEntry : ds ) {
+				System.out.println("Entry: " + currentEntry.getFileName());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
